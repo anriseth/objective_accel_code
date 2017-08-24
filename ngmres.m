c1883e9ec2e26a31e11bc63316a92ef08ca86b1b
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2017-08-24 10:29:23 +0100

    Original code

---
+++ ngmres.m
@@ -0,0 +1,237 @@
+function out=ngmres(u0,fg,M,linesearch,par)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%NGMRES Nonlinear GMRES optimization method
+%
+% out=NGMRES(u0,fg,M,linesearch,par) minimizes a function using the nonlinear GMRES
+%   (N-GMRES) optimization method.
+%
+%   The method was proposed and analyzed in the following references:
+%      -Hans De Sterck, "A Nonlinear GMRES Optimization Algorithm for Canonical
+%       Tensor Decomposition", SIAM J. Sci. Comput. 34, A1351-A1379, 2012.
+%      -Hans De Sterck, "Steepest Descent Preconditioning for Nonlinear GMRES
+%       Optimization", Numerical Linear Algebra with Applications 20, 453-471, 2013.
+% 
+% INPUT:
+% u0: initial guess (column vector)
+% [f,g] = fg(u): fg is a function handle to a function that computes
+%   function value f and gradient vector g in point u
+% [u_new,f_new,g_new,fev] = M(u_old,f,g): M is a function handle to a
+%   function that computes a new approximation u_new from an old
+%   approximation u_old; M is the preconditioner of the n-gmres method;
+%   f and g are the function and gradient in u_old, and f_new and g_new in
+%   u_new; fev is the number of f and g evaluations
+% [u,f,g,step,fev] = linesearch(fg,u0,f0,g0,d): linesearch is a function handle to
+%   a function that does a line search minimizing f starting from u0 in
+%   search direction d; f, g and step are the function value, gradient
+%   vector and step length at the computed point u; f0 and g0 are the
+%   function and gradient vector at u0, and fev is the number of f and g
+%   evaluations during the line search
+% par: parameters for ngmres
+%   par.w : maximum window size
+%   par.maxIt : maximum number of iterations
+%   par.relfTol : stopping tolerance on relative change in f
+%   par.absgTol : stopping tolerance on the 2-norm of g
+%   par.verbose : level of output to screen (0=no output, 1=summary output,
+%       2=detailed output for each iteration)
+%   par.logfev : flag to include the history of the number of f and g
+%       evaluations in the output (true or false)
+%   par.logf : flag to include the history of function values in the output
+%       (true or false)
+%   par.logg : flag to include the history of the 2-norm of the gradient
+%       vectors in the output (true or false)
+%   par.logRestart : flag to include the history of the restarts (true or
+%       false)
+%
+% OUTPUT:
+% out: ngmres output
+%   out.u : computed solution point
+%   out.fev : history of number of f anf g evaluations (optional)
+%   out.logf : history of function values (optional)
+%   out.logg : history of the 2-norm of the gradient vectors (optional)
+%   out.logRestart : restart history (indicator array) (optional)
+%   out.convFlag : 1 = relfTol or absgTol convergence tolerances were
+%                       satisfied
+%                  2 = maxIt was reached without convergence
+%                  0 = exit for other reason
+%
+%  See also NGMRES_TEST_GENERAL, CP_NGMRES, NGMRES_TEST_TENSOR_CP
+%
+%by Hans De Sterck, September 2011
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% some fixed parameters used in the algorithm
+epsi=1e-12; % parameter used in regularizing the normal equation system
+
+% some initialization
+n=size(u0,1); % number of components in the u vectors
+U=zeros(n,par.w); % solution vectors in the window
+R=zeros(n,par.w); % gradient vectors in the window
+logRestart=zeros(par.maxIt,1);
+
+cnt=0; % counter for the number of iterations
+nfev=0; % counter for the number of f and g evaluations
+
+u=u0; % initialize the current iterate, u
+[f,g]=fg(u); % compute function value and gradient in u
+nfev=nfev+1;
+ng=norm(g);
+
+% check convergence criteria
+finishedIt= cnt >= par.maxIt;
+finishedTol= ng <= par.absgTol ;
+finishedCrash=false;
+
+while ~(finishedIt | finishedTol | finishedCrash)
+    % start a new window
+    logRestart(cnt+1)=1;
+    if par.verbose==2
+        disp('*** restart')
+    end
+    U(:,1)=u; % the current iterate is the first vector in the new window
+    R(:,1)=g; % also store its gradient
+    Q(1,1)=g'*g;
+
+    restart=0; % no restart for now
+    curw=1; % current window size (starts at 1, grows up to par.w)
+    
+    % start a sequence of accelerated updates, build towards window size w, but exit if a restart is required
+    cntLoc=0; % counter of number of iterations since the last restart
+    while restart==0
+        cnt=cnt+1; % the very first new aproximation has cnt=1 (the initial guess is not counted) (but it is part of the window for acceleration!)
+        cntLoc=cntLoc+1;
+        
+        f_previous=f; % save the function value of the previous iterate for convergence test
+
+        % STEP I: get a new unaccelerated iterate
+        %-------------------------------------------------
+        [u_new,f_new,g_new,fev]=M(u,f,g);
+        nfev=nfev+fev;
+        
+        % STEP II: compute the n-gmres accelerated iterate
+        %-------------------------------------------------
+        % compute its function value and gradient vector
+        
+        % form the least-squares system
+        % efficiently by storing some previous inner products; see Washio
+        % and Oosterlee, ETNA 6, pp. 271-290, 1997
+        
+        eta=g_new'*g_new;
+        for i=1:curw % iterations in window
+            ksi(i)=g_new'*R(:,i);
+            beta(i,1)=eta-ksi(i);
+        end
+        for i=1:curw % iterations in window
+            for j=1:curw % iterations in window
+                Mat(i,j)=Q(i,j)-ksi(i)-ksi(j)+eta;
+            end
+        end        
+                
+        %delta=epsi*max(diag(Mat)+1e-16);
+        delta=epsi*max(max(diag(Mat)),epsi); % the outer max is to avoid delta=0, which may occur if Mat=0, e.g. at numerical convergence
+
+        % solve the normal equation system        
+        alpha=(Mat(1:curw,1:curw)+delta*eye(size(Mat(1:curw,1:curw),1))) \ beta(1:curw);
+        
+        % we could alternatively use Matlab's \ to solve the LS system...
+        %matlabLS=0;
+        %if matlabLS==1
+        %    alpha=-Z\g_new;
+        %end
+        
+        if isnan(norm(alpha)) % this does not seem to happen, but just in case
+            finishedCrash=true;
+            disp('+++++ WARNING: ngmres exit due to NaN')
+        end
+        
+        % compute the accelerated approximation
+        coef=1-sum(alpha);            
+        u_a=coef*u_new;
+
+        for w=1:curw
+            u_a=u_a+alpha(w)*U(:,w);
+        end
+        
+        % STEP III: do a line search for globalization
+        %-------------------------------------------------
+        d=u_a-u_new; % search directory for the line search
+        % we'll do a restart if d is not a descent direction: 
+        % (d is a descent direction if d * g_new < 0)
+        % (recal that g_new is the gradient, which is the direction of steepest ascent)
+        if d' * g_new >= 0
+                restart=1;
+        end
+        [u,f,g,step,fev] = linesearch(fg,u_new,f_new,g_new,d);
+        % note: line search will normally return u_new if d is not a
+        % descent direction (depending on the line search implementation)
+        nfev=nfev+fev;
+
+        % provide some output and get some log information
+        ng=norm(g);
+        if par.verbose==2
+            fprintf('it %d g %16.16g fev %d f %16.16g step %16.16g alpha %16.16g g_new %16.16g f_new %16.16g \n',...
+                cnt,ng,fev,f,step,norm(alpha),norm(g_new),f_new);
+        end
+        
+        if par.logfev
+            logfev(cnt)=nfev;
+        end
+        
+        if par.logf
+            logf(cnt)=f;
+        end
+        
+        if par.logg
+            logg(cnt)=ng;
+        end
+
+        if curw<par.w
+            curw=curw+1;
+        end
+        
+        if restart==0 % no restart, so update U and R, and Q (to store previous inner products)
+            % which column j of U and R to update
+            j=mod(cntLoc,par.w)+1;
+            U(:,j)=u;
+            R(:,j)=g;
+            for i=1:curw
+                Q(j,i)=g'*R(:,i);
+                Q(i,j)=Q(j,i);
+            end
+        end
+
+        % check the stopping criteria
+        finishedIt= cnt >= par.maxIt;
+        finishedTol= ng <= par.absgTol | abs(f-f_previous)/f_previous <= par.relfTol ;
+        if finishedIt | finishedTol | finishedCrash
+            break % break out of the inner while loop when one of the convergence criteria is satisfied
+        end
+
+    end
+end
+
+% prepare the output data
+out.u=u;
+if par.logfev
+    out.logfev=logfev;
+end
+if par.logf
+    out.logf=logf;
+end
+if par.logg
+    out.logg=logg;
+end
+if par.logRestart==true
+    logRestart=logRestart(1:cnt);
+    out.logRestart=logRestart;
+end
+
+out.convFlag=0; % finished because of crash
+if finishedTol
+    out.convFlag=1; % one of the convergence tolerances was reached
+elseif finishedIt
+    out.convFlag=2; % finished because maximum number of iterations was reached
+end
+
+if par.verbose==1
+    fprintf('ngmres finished: it %d g %16.16g f %16.16g\n',cnt,ng,f);
+end
---
+++ ngmres_test_tensor_CP.m
@@ -0,0 +1,329 @@
+function ngmres_test_tensor_CP
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%NGMRES_TEST_TENSOR_CP is a simple test script applying the N-GMRES
+%optimization method with ALS preconditioning to a canonical tensor
+%decomposition problem, and comparing with other optimization algorithms
+%(NCG and ALS)
+%
+%   This function requires the Tensor toolbox (version 2.5) and the Poblano
+%   toolbox. (http://www.sandia.gov/~tgkolda/TensorToolbox)
+%
+%   The nonlinear GMRES (N-GMRES) optimization method was proposed and analyzed
+%   in the following references:
+%      -Hans De Sterck, "A Nonlinear GMRES Optimization Algorithm for Canonical
+%       Tensor Decomposition", SIAM J. Sci. Comput. 34, A1351-A1379, 2012.
+%      -Hans De Sterck, "Steepest Descent Preconditioning for Nonlinear GMRES
+%       Optimization", Numerical Linear Algebra with Applications 20, 453-471, 2013.
+%
+%   The preconditioner chosen for the N-GMRES algorithm is ALS here, and
+%   poblano_linesearch from the Poblano toolbox is used for the line
+%   searches.
+%   NGMRES_TEST_TENSOR_CP calls the N-GMRES optimization method as
+%   implemented in ngmres.m (via a call to CP_NGMRES).
+%
+%   The tensor test problem considered here is the problem from Tomasi and
+%   Bro (2006) and Acar et al. (2011) for a ramdom dense tensor modified to
+%   get specified collinearity between the columns.
+%
+%   See also NGMRES, CP_NGMRES, NGMRES_TEST_GENERAL, TENSOR, SPTENSOR,
+%   KTENSOR, CP_OPT, NCG, POBLANO_LINESEARCH.
+%
+%by Hans De Sterck, September 2011
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+disp('*********** NEW RUN ***********')
+clear all;
+%------------------------------
+% parameters
+%------------------------------
+% some general parameters
+compareNCG=1;
+compareALS=1;
+figStart=0;
+initSeed=0;
+s = RandStream('mt19937ar','Seed', initSeed);
+RandStream.setDefaultStream(s);
+
+% ngmres parameters
+par.w=20;      % maximum window size
+par.maxIt=200; % maximum number of iterations
+par.relfTol=-1;% stopping tolerance on relative change in f
+par.absgTol=-1;% stopping tolerance on the 2-norm of g
+par.verbose=2; % level of output to screen (0=no output, 1=summary output,
+               %       2=detailed output for each iteration)
+par.logfev=true; % flag to include the history of the number of f and g
+                 %       evaluations in the output (true or false)
+par.logf=true; % flag to include the history of functiona values in the output
+               %       (true or false)
+par.logg=true; % flag to include the history of the 2-norm of the gradient
+par.logRestart=true; % flag to include the history of the restarts
+
+% ngmres line search parameters (for poblano_linesearch)
+par_ls.LineSearch_ftol=1.000000000000000e-04;
+par_ls.LineSearch_gtol=0.010000000000000;
+par_ls.LineSearch_initialstep=1;
+par_ls.LineSearch_maxfev=20;
+par_ls.LineSearch_method='more-thuente';
+par_ls.LineSearch_stpmax=1.000000000000000e+15;
+par_ls.LineSearch_stpmin=1.000000000000000e-15;
+par_ls.LineSearch_xtol=1.000000000000000e-15;
+
+% ncg parameters (for comparing ncg with ngmres)
+options=ncg('defaults');
+options.MaxFuncEvals=2000;
+options.MaxIters=par.maxIt;
+options.TraceFunc=true;
+options.TraceGradNorm=true;
+options.RelFuncTol=-1;
+options.StopTol=1e-99;
+options.Display='iter'; % iter, final or off
+options.RestartNW=false;
+options.Update='PR'; % 'PR', 'FR', 'HS' or 'SD'
+options.LineSearch_maxfev=20; % default: 20
+options.LineSearch_ftol=1e-4; % default: 1e-4
+options.LineSearch_gtol=1e-2; % default: 1e-2
+options.LineSearch_initialstep=1;
+
+%------------------------------
+% create a test tensor
+% (this is the test from Tomasi and Bro (2006) and Acar et al. (2011) for
+% a ramdom dense tensor modified to get specified collinearity between the
+% columns)
+%------------------------------
+s=50; % size of each mode
+r=3; % number of rank-one components in the original noise-free random tensor 
+l1=1; % magnitude of the first type of noise
+l2=1; % magnitude of the second type of noise
+c=0.9; % collinearity
+pars=[s c r l1 l2];
+T=can_createTensorDense(pars);
+
+%--------------------------------------
+% do some pre-processing
+%--------------------------------------
+nT2=norm(T)^2;
+
+%--------------------------------------
+% generate the random initial guess
+%--------------------------------------
+u0=rand(r*sum(size(T)),1);
+
+%--------------------------------------
+% call cp_ngmres
+%--------------------------------------
+out=cp_ngmres(T,r,tt_cp_vec_to_fac(u0,T),par,par_ls);
+
+%--------------------------------------
+% call ncg for comparison       
+%--------------------------------------
+if compareNCG==1
+    disp('+++ start n-cg directly')
+    out_ncg=ncg(@(u) tt_cp_fun(u,T,nT2),u0,options);
+end
+
+%--------------------------------------
+% compare with ALS       
+%--------------------------------------
+if compareALS==1
+    disp('+++ start ALS')
+    u=u0;
+    [f g]=tt_cp_fun(u,T,nT2);
+    for k=1:par.maxIt
+        [u,f,g,fev]=can_ALSu(T,r,u,f,g,@(u) tt_cp_fun(u,T,nT2));
+        fALS(k)=f;
+        gALS(k)=norm(g);        
+    end
+end
+
+%-----------------------------------------
+% some figure output
+%--------------------------------------
+if par.logg
+    figure(figStart+1)
+    semilogy(out.logg,'-+')
+    title('convergence of the norm of the gradient')
+    if par.logRestart
+        loggMod=out.logg;
+        loggMod(out.logRestart==0)=NaN;
+        hold on
+            semilogy(loggMod,'+r')
+        hold off
+    end
+    if compareNCG==1
+        hold on
+            semilogy(out_ncg.TraceGradNorm(2:end),'-*')
+        hold off
+    end
+    if compareALS==1
+        hold on
+            semilogy(gALS,'-')
+        hold off
+    end
+    legend('n-gmres','restarts','n-cg','ALS')
+    xlabel('iterations')
+end
+
+if par.logf
+    figure(figStart+2)
+    semilogy(out.logf-min(out.logf),'-+')
+    title('convergence towards the minimum value of f')
+    if par.logRestart
+        logfMod=out.logf-min(out.logf);
+        logfMod(out.logRestart==0)=NaN;
+        hold on
+            semilogy(logfMod,'+r')
+        hold off
+    end
+    if compareNCG==1
+        minval=min(min(out.logf),min(out_ncg.TraceFunc(2:end)));
+        hold on
+            semilogy(out_ncg.TraceFunc(2:end)-minval,'-*')
+        hold off
+    end
+    if compareALS==1
+        minval=min(min(out.logf),min(fALS));
+        hold on
+            semilogy(fALS-minval,'-')
+        hold off
+    end
+    legend('n-gmres','restarts','n-cg','ALS')
+    xlabel('iterations')
+end
+
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [u,f,g,fev]=can_ALSu(T,r,u,f,g,fg);   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%this function performs one ALS iteration (it uses a modification of the
+%ALS routine in the tensor toolbox (only one iteration, and normalization
+%is different))
+%
+%by Hans De Sterck, September 2011
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Extract number of dimensions and norm of T.
+nModes = ndims(T);
+dims=size(T);
+
+% convert u to a ktensor
+A = tt_cp_vec_to_fac(u,T);
+A=ktensor(A);
+
+A=can_ALS(T,r,A);
+
+% convert A to a vector
+u = tt_fac_to_vec(A.U);
+
+% assume ALS is equivalent to 2 f and g evaluations (in reality it is a
+% bit less expensive I think)
+fev=2;
+
+[f g]=fg(u);
+fev=fev+1;
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function A=can_ALS(T,r,A);   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%this function performs one ALS iteration
+%this is a modification of the ALS routine in the tensor toolbox (only one
+%iteration, and normalization is different)
+%
+%by Hans De Sterck, September 2011
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Extract number of dimensions and norm of T.
+nModes = ndims(T);
+
+% if there is a lambda factor in A, put it into the first mode
+lambda=A.lambda;
+A.U{1} = A.U{1} * spdiags(lambda,0,r,r);
+A.lambda=ones(r,1);
+
+% Iterate over all modes of the tensor
+for n = 1:nModes
+    % Calculate Unew = X_(n) * khatrirao(all U except n, 'r').
+    Unew = mttkrp(T,A.U,n);
+
+    % Compute the matrix of coefficients for linear system
+    Y = ones(r,r);
+    for i = [1:n-1,n+1:nModes]
+        Y = Y .* (A.U{i}'*A.U{i});
+    end
+
+    Unew = (Y \ Unew')'; %<- Line from TTB 2.2.
+
+    if issparse(Unew)
+      A.U{n} = full(Unew);   % for the case R=1
+    else
+      A.U{n} = Unew;
+    end
+end
+
+A = ktensor(A.U); % default: lambda=1
+
+A=arrange(A); % normalize and introduce lambda, and make sure the components appear in order of lambda
+
+% now distribute lambda evenly over all modes
+facMat=spdiags(nthroot(A.lambda,nModes),0,r,r);
+for n=1:nModes
+    A.U{n} = A.U{n} * facMat;
+end
+
+A = ktensor(A.U); % lambda=1 by default
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function Zdprime=can_createTensorDense(params)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%this function creates a test tensor for CP decomposition
+%(this is the test from Tomasi and Bro (2006) and Acar et al. (2011) for
+%a ramdom dense tensor modified to get specified collinearity between the
+%columns)
+%
+%by Hans De Sterck, September 2011
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+sizet=params(1);
+c=params(2);
+rTrue=params(3);
+l1=params(4);
+l2=params(5);
+
+% first generate K
+K=ones(rTrue,rTrue)*c;
+for k=1:rTrue
+    K(k,k)=1;
+end
+K;
+
+% get C as the cholesky factor of K
+C=chol(K);
+
+for n=1:3
+    % generate a random matrix
+    M=randn(sizet,rTrue);
+    % ortho-normalize the columns of M, gives Q
+    [Q,R] = qr(M,0);    
+    U{n}=Q*C;
+end
+
+Z=full(ktensor(U));
+
+% generate two random tensors
+N1=tensor(randn(sizet,sizet,sizet));
+N2=tensor(randn(sizet,sizet,sizet));
+nZ=norm(Z);
+nN1=norm(N1);
+
+% modify Z with the two different types of noise
+Zprime=Z+1/sqrt(100/l1-1)*nZ/nN1*N1;
+nZprime=norm(Zprime);
+
+N2Zprime=N2.*Zprime;
+nN2Zprime=norm(N2Zprime);
+
+Zdprime=Zprime+1/sqrt(100/l2-1)*nZprime/nN2Zprime*N2Zprime;
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
