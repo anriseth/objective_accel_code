d999a1879cead464152a441cf8e943535778ed43
Asbjørn Nilsen Riseth <anriseth@gmail.com>
2017-08-25 09:37:47 +0100

    Shorter names

--- get_par.m
+++ get_par.m
@@ -13,24 +13,24 @@ function par = get_par(seednum)
     %------------------------------
     % some general parameters
     % first select which methods to compare
-    par.compareNGMRES_sdls_precond=1;       % run 1
-    par.compareNGMRES_descent_precond=1;    % run 2
-    par.compareNCG=1;                       % run 4
-    par.compare_sdls_precond=0;             % run 5
-    par.compareLBFGS=1;                     % run 7
-    par.compareNGMRESO_sdls_precond=1;    % run 8
-    par.compareNGMRESO_descent_precond=1; % run 9
-                                          %
+    par.compareNGMRES_sdls=1;  % run 1
+    par.compareNGMRES_sd=1;    % run 2
+    par.compareNCG=1;          % run 4
+    par.compare_sdls=0;        % run 5
+    par.compareLBFGS=1;        % run 7
+    par.compareNGMRESO_sdls=1; % run 8
+    par.compareNGMRESO_sd=1;   % run 9
+                               %
     par.initSeed = seednum;
-    
-    par.figRunFirst=7; % which run to plot first in the figures (needs to be selected above)
-    par.figStart=100;  % figure number
-    par.precStep1=1e-4; % step in steepest descent preconditioner without line search
-    par.precStep2=1e0;  % step factor in steepest descent preconditioner without line search
-    par.maxfev=20;      % maximum function evaluations in all line searches
+
+    par.figRunFirst=7;       % which run to plot first in the figures (needs to be selected above)
+    par.figStart=100;        % figure number
+    par.precStep1=1e-4;      % step in steepest descent preconditioner without line search
+    par.precStep2=1e0;       % step factor in steepest descent preconditioner without line search
+    par.maxfev=20;           % maximum function evaluations in all line searches
     par.restartfigure=false; % indicate restarts on the pictures
-    par.epsi=1e-16;     % for plotting convergence to f*
-                        % line styles
+    par.epsi=1e-16;          % for plotting convergence to f*
+                             % line styles
     par.fs{1}=':k';
     par.fs{2}='-k';
     par.fs{4}='-.k';
@@ -47,17 +47,17 @@ function par = get_par(seednum)
     par.mispace{9} = 10;
 
     % ngmres parameters
-    par.par_ngmres.w=20;       % maximum window size
-    par.par_ngmres.maxIt=200;  % maximum number of iterations
-    par.par_ngmres.relfTol=-1; % stopping tolerance on relative change in f
-    par.par_ngmres.absgTol=-1; % stopping tolerance on the 2-norm of g
-    par.par_ngmres.verbose=2;  % level of output to screen (0=no output, 1=summary output,
-                               %       2=detailed output for each iteration)
-    par.par_ngmres.logfev=true;% flag to include the history of the number of f and g
-                               %       evaluations in the output (true or false)
-    par.par_ngmres.logf=true;  % flag to include the history of functiona values in the output
-                               %       (true or false)
-    par.par_ngmres.logg=true;  % flag to include the history of the 2-norm of the gradient
+    par.par_ngmres.w=20;            % maximum window size
+    par.par_ngmres.maxIt=1500;      % maximum number of iterations
+    par.par_ngmres.relfTol=-1;      % stopping tolerance on relative change in f
+    par.par_ngmres.absgTol=-1;      % stopping tolerance on the 2-norm of g
+    par.par_ngmres.verbose=0;       % level of output to screen (0=no output, 1=summary output,
+                                    %       2=detailed output for each iteration)
+    par.par_ngmres.logfev=true;     % flag to include the history of the number of f and g
+                                    %       evaluations in the output (true or false)
+    par.par_ngmres.logf=true;       % flag to include the history of functiona values in the output
+                                    %       (true or false)
+    par.par_ngmres.logg=true;       % flag to include the history of the 2-norm of the gradient
     par.par_ngmres.logRestart=true; % flag to include the history of the restarts
 
     % ngmres line search parameters (for poblano_linesearch)
@@ -79,17 +79,17 @@ function par = get_par(seednum)
     par.par_ncg.TraceGradNorm=true;
     par.par_ncg.RelFuncTol=-1;
     par.par_ncg.StopTol=1e-300;
-    par.par_ncg.Display='iter'; % iter, final or off
+    par.par_ncg.Display='off';                % iter, final or off
     par.par_ncg.RestartNW=false;
-    par.par_ncg.Update='PR'; % 'PR', 'FR', 'HS' or 'SD'
+    par.par_ncg.Update='PR';                  % 'PR', 'FR', 'HS' or 'SD'
     par.par_ncg.LineSearch_maxfev=par.maxfev; % default: 20
-    par.par_ncg.LineSearch_ftol=1e-4; % default: 1e-4
-    par.par_ncg.LineSearch_gtol=1e-2; % default: 1e-2
+    par.par_ncg.LineSearch_ftol=1e-4;         % default: 1e-4
+    par.par_ncg.LineSearch_gtol=1e-2;         % default: 1e-2
     par.par_ncg.LineSearch_initialstep=1;
 
     % lbfgs parameters (for comparing lbfgs with ngmres)
     par.par_lbfgs=lbfgs('defaults');
-    par.par_lbfgs.M=5; % window size
+    par.par_lbfgs.M=5;                          % window size
     par.par_lbfgs.MaxFuncEvals=50000;
     par.par_lbfgs.MaxIters=par.par_ngmres.maxIt;
     par.par_lbfgs.TraceFuncEvals=true;
@@ -97,19 +97,19 @@ function par = get_par(seednum)
     par.par_lbfgs.TraceGradNorm=true;
     par.par_lbfgs.RelFuncTol=-1;
     par.par_lbfgs.StopTol=1e-300;
-    par.par_lbfgs.Display='iter'; % iter, final or off
+    par.par_lbfgs.Display='off';                % iter, final or off
     par.par_lbfgs.LineSearch_maxfev=par.maxfev; % default: 20
-    par.par_lbfgs.LineSearch_ftol=1e-4; % default: 1e-4
-    par.par_lbfgs.LineSearch_gtol=1e-2; % default: 1e-2
+    par.par_lbfgs.LineSearch_ftol=1e-4;         % default: 1e-4
+    par.par_lbfgs.LineSearch_gtol=1e-2;         % default: 1e-2
     par.par_lbfgs.LineSearch_initialstep=1;
 
     % steepest decent line search parameters (for using SD as a preconditioner)
-    par.par_sdls_precond.LineSearch_ftol=1.000000000000000e-04;
-    par.par_sdls_precond.LineSearch_gtol=0.010000000000000;
-    par.par_sdls_precond.LineSearch_initialstep=1;
-    par.par_sdls_precond.LineSearch_maxfev=par.maxfev;
-    par.par_sdls_precond.LineSearch_method='more-thuente';
-    par.par_sdls_precond.LineSearch_stpmax=1.000000000000000e+15;
-    par.par_sdls_precond.LineSearch_stpmin=1.000000000000000e-15;
-    par.par_sdls_precond.LineSearch_xtol=1.000000000000000e-15;
+    par.par_sdls.LineSearch_ftol=1.000000000000000e-04;
+    par.par_sdls.LineSearch_gtol=0.010000000000000;
+    par.par_sdls.LineSearch_initialstep=1;
+    par.par_sdls.LineSearch_maxfev=par.maxfev;
+    par.par_sdls.LineSearch_method='more-thuente';
+    par.par_sdls.LineSearch_stpmax=1.000000000000000e+15;
+    par.par_sdls.LineSearch_stpmin=1.000000000000000e-15;
+    par.par_sdls.LineSearch_xtol=1.000000000000000e-15;
 end
--- util_ffigure_evals.m
+++ util_ffigure_evals.m
@@ -11,53 +11,53 @@ function util_ffigure_evals(par,out)
     % convergence of f to the minimum value achieved in any of the tests)
     switch par.figRunFirst
       case 1
-        minval=min(out.out_ngmres_sdls_precond.logf);
+        minval=min(out.out_ngmres_sdls.logf);
       case 2
-        minval=min(out.out_ngmres_descent_precond.logf);
+        minval=min(out.out_ngmres_sd.logf);
       case 4
         minval=min(out.out_ncg.TraceFunc(2:end));
       case 5
-        minval=min(out.out_sdls_precond.logf);
+        minval=min(out.out_sdls.logf);
       case 7
         minval=min(out.out_lbfgs.TraceFunc(2:end));
       case 8
-        minval=min(out.out_ngmreso_sdls_precond.logf);
+        minval=min(out.out_ngmreso_sdls.logf);
       case 9
-        minval=min(out.out_ngmreso_descent_precond.logf);
+        minval=min(out.out_ngmreso_sd.logf);
     end
-    if par.compareNGMRES_sdls_precond==1 & par.figRunFirst~=1
-        minval=min(minval,min(out.out_ngmres_sdls_precond.logf));
+    if par.compareNGMRES_sdls==1 & par.figRunFirst~=1
+        minval=min(minval,min(out.out_ngmres_sdls.logf));
     end
-    if par.compareNGMRES_descent_precond==1 & par.figRunFirst~=2
-        minval=min(minval,min(out.out_ngmres_descent_precond.logf));
+    if par.compareNGMRES_sd==1 & par.figRunFirst~=2
+        minval=min(minval,min(out.out_ngmres_sd.logf));
     end
     if par.compareNCG==1 & par.figRunFirst~=4
         minval=min(minval,min(out.out_ncg.TraceFunc(2:end)));
     end
-    if par.compare_sdls_precond==1 & par.figRunFirst~=5
-        minval=min(minval,min(out.out_sdls_precond.logf));
+    if par.compare_sdls==1 & par.figRunFirst~=5
+        minval=min(minval,min(out.out_sdls.logf));
     end
     if par.compareLBFGS==1 & par.figRunFirst~=7
         minval=min(minval,min(out.out_lbfgs.TraceFunc(2:end)));
     end
-    if par.compareNGMRESO_sdls_precond==1 & par.figRunFirst~=8
-        minval=min(minval,min(out.out_ngmreso_sdls_precond.logf));
+    if par.compareNGMRESO_sdls==1 & par.figRunFirst~=8
+        minval=min(minval,min(out.out_ngmreso_sdls.logf));
     end
-    if par.compareNGMRESO_descent_precond==1 & par.figRunFirst~=9
-        minval=min(minval,min(out.out_ngmreso_descent_precond.logf));
+    if par.compareNGMRESO_sd==1 & par.figRunFirst~=9
+        minval=min(minval,min(out.out_ngmreso_sd.logf));
     end
 
     % then make the plots
     switch par.figRunFirst
       case 1
-        var = out.out_ngmres_sdls_precond;
+        var = out.out_ngmres_sdls;
         cnum = 1;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
                  par.fs{cnum},'LineWidth',1,'MarkerIndices',mind)
         legendstr{nCurves}='N-GMRES-sdls';
       case 2
-        var = out.out_ngmres_descent_precond;
+        var = out.out_ngmres_sd;
         cnum = 2;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
@@ -71,7 +71,7 @@ function util_ffigure_evals(par,out)
                  par.fs{cnum},'LineWidth',1,'MarkerIndices',mind)
         legendstr{nCurves}='N-CG';
       case 5
-        var = out.out_sdls_precond;
+        var = out.out_sdls;
         cnum = 5;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
@@ -85,23 +85,23 @@ function util_ffigure_evals(par,out)
                  par.fs{cnum},'LineWidth',1,'MarkerIndices',mind)
         legendstr{nCurves}='L-BFGS';
       case 8
-        var = out.out_ngmreso_sdls_precond;
+        var = out.out_ngmreso_sdls;
         cnum = 8;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
                  par.fs{cnum},'LineWidth',1,'MarkerIndices',mind)
         legendstr{nCurves}='N-GMRES-O-sdls';
       case 9
-        var = out.out_ngmreso_descent_precond;
+        var = out.out_ngmreso_sd;
         cnum = 9;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
                  par.fs{cnum},'LineWidth',1,'MarkerIndices',mind)
         legendstr{nCurves}='N-GMRES-O-sd';
     end
-    if par.compareNGMRES_sdls_precond==1 & par.figRunFirst~=1
+    if par.compareNGMRES_sdls==1 & par.figRunFirst~=1
         hold on
-        var = out.out_ngmres_sdls_precond;
+        var = out.out_ngmres_sdls;
         cnum = 1;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
@@ -110,9 +110,9 @@ function util_ffigure_evals(par,out)
         nCurves=nCurves+1;
         legendstr{nCurves}='N-GMRES-sdls';
     end
-    if par.compareNGMRES_descent_precond==1 & par.figRunFirst~=2
+    if par.compareNGMRES_sd==1 & par.figRunFirst~=2
         hold on
-        var = out.out_ngmres_descent_precond;
+        var = out.out_ngmres_sd;
         cnum = 2;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
@@ -132,9 +132,9 @@ function util_ffigure_evals(par,out)
         nCurves=nCurves+1;
         legendstr{nCurves}='N-CG';
     end
-    if par.compare_sdls_precond==1 & par.figRunFirst~=5
+    if par.compare_sdls==1 & par.figRunFirst~=5
         hold on
-        var = out.out_sdls_precond;
+        var = out.out_sdls;
         cnum = 5;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
@@ -154,9 +154,9 @@ function util_ffigure_evals(par,out)
         nCurves=nCurves+1;
         legendstr{nCurves}='L-BFGS';
     end
-    if par.compareNGMRESO_sdls_precond==1 & par.figRunFirst~=8
+    if par.compareNGMRESO_sdls==1 & par.figRunFirst~=8
         hold on
-        var = out.out_ngmreso_sdls_precond;
+        var = out.out_ngmreso_sdls;
         cnum = 8;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
@@ -165,9 +165,9 @@ function util_ffigure_evals(par,out)
         nCurves=nCurves+1;
         legendstr{nCurves}='N-GMRES-O-sdls';
     end
-    if par.compareNGMRESO_descent_precond==1 & par.figRunFirst~=9
+    if par.compareNGMRESO_sd==1 & par.figRunFirst~=9
         hold on
-        var = out.out_ngmreso_descent_precond;
+        var = out.out_ngmreso_sd;
         cnum = 9;
         mind = 1:par.mispace{cnum}:length(var.logf);
         semilogy(var.logfev,var.logf-minval+par.epsi, ...
@@ -179,16 +179,16 @@ function util_ffigure_evals(par,out)
 
     % optionally indicate the restart points in red for the N-GMRES runs
     if par.par_ngmres.logRestart & par.restartfigure
-        if par.compareNGMRES_sdls_precond==1
-            logfMod=out.out_ngmres_sdls_precond.logf-minval;
-            logfMod(out.out_ngmres_sdls_precond.logRestart==0)=NaN;
+        if par.compareNGMRES_sdls==1
+            logfMod=out.out_ngmres_sdls.logf-minval;
+            logfMod(out.out_ngmres_sdls.logRestart==0)=NaN;
             hold on
             semilogy(logfMod+par.epsi,'or')
             hold off
         end
-        if par.compareNGMRES_descent_precond==1
-            logfMod=out.out_ngmres_descent_precond.logf-minval;
-            logfMod(out.out_ngmres_descent_precond.logRestart==0)=NaN;
+        if par.compareNGMRES_sd==1
+            logfMod=out.out_ngmres_sd.logf-minval;
+            logfMod(out.out_ngmres_sd.logRestart==0)=NaN;
             hold on
             semilogy(logfMod+par.epsi,'+r')
             hold off
